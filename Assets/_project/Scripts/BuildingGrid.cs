f928004fd451a3da975713948a16afd4f8c7716e
nikistika <miz0@bk.ru>
2025-08-16 11:09:31 +0300

    Skr

--- Assets/_project/Scripts/BuildingGrid.cs
+++ Assets/_project/Scripts/BuildingGrid.cs
@@ -1,49 +1,88 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class BuildingGrid : MonoBehaviour
+
+public class BuildingsGrid : MonoBehaviour
 {
+    public Vector2Int GridSize = new Vector2Int(10, 10);
 
-    [SerializeField] private Vector2Int _gridSize = new Vector2Int(10, 10);
-
-    private Building[,] _grid;
-    private Building _flyingBuilding;
-    private Camera _camera;
+    private Building[,] grid;
+    private Building flyingBuilding;
+    private Camera mainCamera;
     
     private void Awake()
     {
-        _camera = Camera.main;
-        _grid = new Building[_gridSize.x, _gridSize.y];
+        grid = new Building[GridSize.x, GridSize.y];
+        
+        mainCamera = Camera.main;
     }
 
-    void Update()
+    public void StartPlacingBuilding(Building buildingPrefab)
     {
-        if (_flyingBuilding != null)
+        if (flyingBuilding != null)
+        {
+            Destroy(flyingBuilding.gameObject);
+        }
+        
+        flyingBuilding = Instantiate(buildingPrefab);
+    }
+
+    private void Update()
+    {
+        if (flyingBuilding != null)
         {
             var groundPlane = new Plane(Vector3.up, Vector3.zero);
-            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
             if (groundPlane.Raycast(ray, out float position))
             {
                 Vector3 worldPosition = ray.GetPoint(position);
 
-                _flyingBuilding.transform.position = worldPosition;
+                int x = Mathf.RoundToInt(worldPosition.x);
+                int y = Mathf.RoundToInt(worldPosition.z);
+
+                bool available = true;
+
+                if (x < 0 || x > GridSize.x - flyingBuilding.Size.x) available = false;
+                if (y < 0 || y > GridSize.y - flyingBuilding.Size.y) available = false;
+
+                if (available && IsPlaceTaken(x, y)) available = false;
+
+                flyingBuilding.transform.position = new Vector3(x, 0, y);
+                flyingBuilding.SetTransparent(available);
+
+                if (available && Input.GetMouseButtonDown(0))
+                {
+                    PlaceFlyingBuilding(x, y);
+                }
             }
         }
     }
 
-    private void StartPlacingBuilding(Building buildingPrefab)
+    private bool IsPlaceTaken(int placeX, int placeY)
     {
-        if (_flyingBuilding == null)
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
         {
-            Destroy(_flyingBuilding);
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                if (grid[placeX + x, placeY + y] != null) return true;
+            }
         }
 
-        _flyingBuilding = Instantiate(buildingPrefab);
-
+        return false;
     }
-    
+
+    private void PlaceFlyingBuilding(int placeX, int placeY)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                grid[placeX + x, placeY + y] = flyingBuilding;
+            }
+        }
+        
+        flyingBuilding.SetNormal();
+        flyingBuilding = null;
+    }
 }
